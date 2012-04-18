34682c91606e2b61714518b7c3f4081a97f6d177
Yaakov Selkowitz <yselkowitz@users.sourceforge.net>
2012-04-18 18:44:43 -0500

    Fix postinstall script

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,2 @@
 /usr/bin/mkdir -p "$(/usr/bin/cygpath $CYGWINFORALL -P)/Cygwin-X"
-/usr/bin/mkshortcut $CYGWINFORALL -P -i /usr/bin/XWin.exe -n "Cygwin-X/MATE Desktop" -a "/usr/bin/bash.exe -l -c \"/usr/bin/dbus-launch mate-session\"" /usr/bin/run.exe
+/usr/bin/mkshortcut $CYGWINFORALL -P -i /usr/bin/XWin.exe -n "Cygwin-X/MATE Desktop" -a "/usr/bin/bash.exe -l -c \"/usr/bin/startx /usr/bin/dbus-launch mate-session\"" /usr/bin/run.exe
